02364f261d10e0018bebbede71ac05438abca102
Nico Coetzee <nicc777@gmail.com>
2023-03-20 06:11:14 +0100

    feat: working on templates

--- templates/utils/factory/animus-extension-template.py
+++ templates/utils/factory/animus-extension-template.py
@@ -19,7 +19,7 @@ EXAMPLES_BASE_PATH = '{}{}examples'.format(
     os.sep
 )
 
-IMPLEMENTATIONS_BASE_PATH = '{}{}examples'.format(
+IMPLEMENTATIONS_BASE_PATH = '{}{}implementations'.format(
     os.getcwd(),
     os.sep
 )
@@ -290,8 +290,54 @@ class AnimusExtensionTemplate(ManifestBase):
 
         variable_cache.store_variable(variable=Variable(name='{}:validated'.format(self._var_name()),logger=self.logger, initial_value=True), overwrite_existing=True)
 
+    def _prep_file_path_variables(self, variable_cache: VariableCache=VariableCache()):
+        variable_cache.store_variable(
+            variable=Variable(
+                name='{}:doc_file'.format(self._var_name()),
+                initial_value='{}{}{}.md'.format(DOC_BASE_PATH, os.sep, self.metadata['name']),
+                ttl=-1,
+                logger=self.logger,
+                mask_in_logs=False
+            ),
+            overwrite_existing=False
+        )
+        variable_cache.store_variable(
+            variable=Variable(
+                name='{}:example_dir'.format(self._var_name()),
+                initial_value='{}{}minimal'.format(EXAMPLES_BASE_PATH, os.sep),
+                ttl=-1,
+                logger=self.logger,
+                mask_in_logs=False
+            ),
+            overwrite_existing=False
+        )
+        variable_cache.store_variable(
+            variable=Variable(
+                name='{}:example_file'.format(self._var_name()),
+                initial_value='{}{}minimal{}{}.yaml'.format(EXAMPLES_BASE_PATH, os.sep, os.sep, self.metadata['name']),
+                ttl=-1,
+                logger=self.logger,
+                mask_in_logs=False
+            ),
+            overwrite_existing=False
+        )
+        variable_cache.store_variable(
+            variable=Variable(
+                name='{}:implementation_file'.format(self._var_name()),
+                initial_value='{}{}{}.py'.format(IMPLEMENTATIONS_BASE_PATH, os.sep, self.metadata['name']),
+                ttl=-1,
+                logger=self.logger,
+                mask_in_logs=False
+            ),
+            overwrite_existing=False
+        )
+
+    def _delete_file_path_variables(self, variable_cache: VariableCache=VariableCache()):
+        pass
+
     def implemented_manifest_differ_from_this_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache())->bool:
         self._validate(variable_cache=variable_cache)
+        self._prep_file_path_variables(variable_cache=variable_cache)
         return False
 
     def apply_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False):
@@ -300,10 +346,15 @@ class AnimusExtensionTemplate(ManifestBase):
             self.log(message='No changes detected', level='info')
             return
         self.log(message='Applying Manifest', level='info')
+        self.log(message='   Implementation Name           : {}'.format(self.metadata['name']), level='info')
+        self.log(message='      Documentation File         : {}'.format(variable_cache.get_value(variable_name='{}:doc_file'.format(self._var_name()))), level='info')
+        self.log(message='      Minimal Example File       : {}'.format(variable_cache.get_value(variable_name='{}:example_file'.format(self._var_name()))), level='info')
+        self.log(message='      Implementation Source File : {}'.format(variable_cache.get_value(variable_name='{}:implementation_file'.format(self._var_name()))), level='info')
         return 
     
     def delete_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False):
         self.log(message='DELETE CALLED', level='info')
+        self._delete_file_path_variables(variable_cache=variable_cache)
         self.log(message='Deleting Manifest', level='info')
         variable_cache.delete_variable(variable_name=self._var_name())
         return 
