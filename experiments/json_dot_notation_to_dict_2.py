3c0722f776f766710042dd8398427ec64f6066c4
Nico Coetzee <nicc777@gmail.com>
2023-03-30 06:13:54 +0200

    feat: working on templates

---
+++ experiments/json_dot_notation_to_dict_2.py
@@ -0,0 +1,92 @@
+import json
+import copy
+
+
+# WORKING
+original_data1 = '''{
+    "kind": "ShellScript",
+    "version": "v1",
+    "metadata": {
+        "name": "shell-script-v1-minimal",
+        "skipDeleteAll": true
+    },
+    "spec": {
+        "shellInterpreter": "sh",
+        "source.type": "inLine",
+        "source.value": "echo \\"Not Yet Implemented\\""
+    }
+}
+'''
+
+# FIXME  NOT WORKING YET
+original_data2 = '''{
+    "kind": "ShellScript",
+    "version": "v1",
+    "metadata": {
+        "name": "shell-script-v1-minimal",
+        "skipDeleteAll": true
+    },
+    "spec": {
+        "shellInterpreter": "sh",
+        "source.type": "inLine",
+        "source.value": "echo \\"Not Yet Implemented\\"",
+        "source.test.alt1": "111",
+        "source.test.alt2": "222"
+    }
+}
+'''
+
+
+template_data = json.loads(original_data2)
+
+
+def add_parent_key_to_dict(current_dict: dict, parent_key: str):
+    new_dict = dict()
+    new_dict[parent_key] = current_dict
+    return new_dict
+
+
+def nest_data(key_dotted_notation: str, value: object)->dict:
+    keys = key_dotted_notation.split('.')
+    idx = 0
+    d = dict()
+    while len(keys) > 0:
+        idx += 1
+        key = keys.pop()
+        if idx == 1:
+            d[key] = value
+            if len(keys) > 1:
+                d = add_parent_key_to_dict(current_dict=d, parent_key=keys[-1])
+        else:
+            d = add_parent_key_to_dict(current_dict=d, parent_key=key)
+            if len(keys) > 1:
+                d = add_parent_key_to_dict(current_dict=d, parent_key=keys[-1])
+    print('-> d={}'.format(d))
+    return d
+
+
+def merge_dicts(A: dict, B: dict)->dict:
+    # FROM https://stackoverflow.com/questions/29241228/how-can-i-merge-two-nested-dictionaries-together (Vivek Sable)
+    for i, j in B.items(): 
+        if i in A:
+            A[i].update(j)
+        else:
+            A[i] = j
+    return A
+
+
+spec = copy.deepcopy(template_data['spec'])
+for dotted_key in list(spec.keys()):
+    nested_data = nest_data(key_dotted_notation=dotted_key, value=spec[dotted_key])
+    spec.pop(dotted_key)
+    spec = merge_dicts(A=copy.deepcopy(spec), B=copy.deepcopy(nested_data))
+
+
+template_data['spec'] = copy.deepcopy(spec)
+
+
+print(json.dumps(template_data))
+
+
+
+
