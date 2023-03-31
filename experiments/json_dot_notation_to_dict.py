fc7bbad9e3a8da781d37b9a0d2ac6a4dcd996e00
Nico Coetzee <nicc777@gmail.com>
2023-03-31 06:47:38 +0200

    feat: working on templates

--- experiments/json_dot_notation_to_dict.py
+++ experiments/json_dot_notation_to_dict.py
@@ -84,10 +84,9 @@ def merge_dicts(A: dict, B: dict)->dict:
 
 output_data = dict()
 for t_field_name, t_field_data in input_data_converted.items():
-    spec = copy.deepcopy(input_data_converted[t_field_name])
     converted_t_field_data = dict()
-    if isinstance(spec, dict):
-        for field_name, field_data in spec.items():
+    if isinstance(t_field_data, dict):
+        for field_name, field_data in t_field_data.items():
             embedded_field = embed_field(dotted_name=field_name, value=copy.deepcopy(field_data))
             for k,v in embedded_field.to_dict().items():
                 if k not in converted_t_field_data:
