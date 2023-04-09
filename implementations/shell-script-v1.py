37fda2dfea51c26722f0e45c1f0f170b04c54b0e
Nico Coetzee <nicc777@gmail.com>
2023-04-09 09:25:37 +0200

    feat: #1 implementing logic

--- implementations/shell-script-v1.py
+++ implementations/shell-script-v1.py
@@ -6,8 +6,6 @@ from pathlib import Path
 import subprocess
 
 
-
-
 class ShellScript(ManifestBase):
     """Executes a shell script.
 
@@ -28,13 +26,31 @@ variable name
     def __init__(self, logger=get_logger(), post_parsing_method: object=None, version: str='v1', supported_versions: tuple=(['v1'])):
         super().__init__(logger=logger, post_parsing_method=post_parsing_method, version=version, supported_versions=supported_versions)
 
-    def implemented_manifest_differ_from_this_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache())->bool:     
-        return False
+    def implemented_manifest_differ_from_this_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache())->bool:
+        current_exit_code = variable_cache.get_value(
+            variable_name='{}:EXIT_CODE'.format(self.metadata['name']),
+            value_if_expired=None,
+            default_value_if_not_found=None,
+            raise_exception_on_expired=False,
+            raise_exception_on_not_found=False
+        )
+        self.log(message='      current_exit_code={}'.format(current_exit_code), level='debug')
+        if current_exit_code is not None:
+            self.log(message='      returning False', level='debug')
+            return False
+        self.log(message='      returning True', level='debug')
+        return True
 
     def apply_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False):
         self.log(message='APPLY CALLED', level='info')
+        if self.implemented_manifest_differ_from_this_manifest(manifest_lookup_function=manifest_lookup_function, variable_cache=variable_cache) is False:
+            self.log(message='   Script already executed', level='info')
+            return
         return 
     
     def delete_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False):
         self.log(message='DELETE CALLED', level='info')
+        variable_cache.delete_variable(variable_name='{}:EXIT_CODE'.format(self.metadata['name']))
+        variable_cache.delete_variable(variable_name='{}:STDOUT'.format(self.metadata['name']))
+        variable_cache.delete_variable(variable_name='{}:STDERR'.format(self.metadata['name']))
         return 
