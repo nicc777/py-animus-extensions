cafbccf11f225c4ad79434705efa9e5a97daf478
Nico Coetzee <nicc777@gmail.com>
2023-04-10 16:59:06 +0200

    feat: #1 implementing logic

--- implementations/shell-script-v1.py
+++ implementations/shell-script-v1.py
@@ -49,11 +49,42 @@ variable name
                     source = self.spec['source']['type']
         return source
 
+    def _load_source_from_spec(self)->str:
+        source = 'exit 0'
+        if 'source' in self.spec:
+            if 'value' in self.spec['source']:
+                source = self.spec['source']['value']
+        return source
+
+    def _load_source_from_file(self)->str:
+        source = 'exit 0'
+        if 'source' in self.spec:
+            if 'value' in self.spec['source']:
+                try:
+                    self.log(message='Loading script source from file "{}"'.format(self.spec['source']['value']), level='info')
+                    with open(self.spec['source']['value'], 'r') as f:
+                        source = f.read()
+                except:
+                    self.log(message='EXCEPTION: {}'.format(traceback.format_exc()), level='error')
+        return source
+
     def apply_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False):
         self.log(message='APPLY CALLED', level='info')
         if self.implemented_manifest_differ_from_this_manifest(manifest_lookup_function=manifest_lookup_function, variable_cache=variable_cache) is False:
             self.log(message='   Script already executed', level='info')
             return
+        script_source = 'exit 0'
+        if self._id_source == 'inline':
+            shabang = '#!/bin/sh'
+            if 'shellInterpreter' in self.spec:
+                shabang = self.spec['shellInterpreter']
+            script_source = '{}\n\n{}'.format(
+                shabang,
+                self._load_source_from_spec()
+            )
+        else:
+            script_source = self._load_source_from_file()
+        
         return 
     
     def delete_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False):
