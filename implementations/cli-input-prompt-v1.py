9a66f3215ef8be6d434c89e343cd1af9adb6e2f8
Nico Coetzee <nicc777@gmail.com>
2023-04-21 04:41:29 +0200

    feat: aligned to v1.0.9 of py-animus

--- implementations/cli-input-prompt-v1.py
+++ implementations/cli-input-prompt-v1.py
@@ -134,7 +134,7 @@ class CliInputPrompt(ManifestBase):
 
         variable_cache.store_variable(variable=Variable(name='{}:validated'.format(self._var_name()),logger=self.logger, initial_value=True), overwrite_existing=True)
 
-    def implemented_manifest_differ_from_this_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache())->bool:
+    def implemented_manifest_differ_from_this_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders())->bool:
         self._validate(variable_cache=variable_cache)        
         current_value = variable_cache.get_value(
             variable_name=self._var_name(),
@@ -149,7 +149,7 @@ class CliInputPrompt(ManifestBase):
         
         return False
 
-    def apply_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False):
+    def apply_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False, target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders()):
         self._validate(variable_cache=variable_cache)
         value = None
         self.log(message='variable_cache={}'.format(str(variable_cache)), level='debug')
@@ -211,6 +211,6 @@ class CliInputPrompt(ManifestBase):
         )
         return 
     
-    def delete_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False):
+    def delete_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False, target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders()):
         variable_cache.delete_variable(variable_name=self._var_name())
         return 
--- implementations/shell-script-v1.py
+++ implementations/shell-script-v1.py
@@ -27,7 +27,7 @@ variable name
     def __init__(self, logger=get_logger(), post_parsing_method: object=None, version: str='v1', supported_versions: tuple=(['v1'])):
         super().__init__(logger=logger, post_parsing_method=post_parsing_method, version=version, supported_versions=supported_versions)
 
-    def implemented_manifest_differ_from_this_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache())->bool:
+    def implemented_manifest_differ_from_this_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders())->bool:
         current_exit_code = variable_cache.get_value(
             variable_name='{}:EXIT_CODE'.format(self.metadata['name']),
             value_if_expired=None,
@@ -99,7 +99,7 @@ variable name
             self.log(message='   EXCEPTION in _create_work_file(): {}'.format(traceback.format_exc()), level='error')
         return work_file
 
-    def apply_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False):
+    def apply_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False, target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders()):
         self.log(message='APPLY CALLED', level='info')
         if self.implemented_manifest_differ_from_this_manifest(manifest_lookup_function=manifest_lookup_function, variable_cache=variable_cache) is False:
             self.log(message='   Script already executed', level='info')
@@ -214,7 +214,7 @@ variable name
         self._del_file(file=work_file)
         return 
     
-    def delete_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False):
+    def delete_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False, target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders()):
         self.log(message='DELETE CALLED', level='info')
         variable_cache.delete_variable(variable_name='{}:EXIT_CODE'.format(self.metadata['name']))
         variable_cache.delete_variable(variable_name='{}:STDOUT'.format(self.metadata['name']))
