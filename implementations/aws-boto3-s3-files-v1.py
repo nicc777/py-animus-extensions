bbd07b7d59681e529d246c60608403705dfdfb5d
Nico Coetzee <nicc777@gmail.com>
2023-05-02 07:57:46 +0200

    feat: #22 - Started adding initial processing logic

--- implementations/aws-boto3-s3-files-v1.py
+++ implementations/aws-boto3-s3-files-v1.py
@@ -53,6 +53,52 @@ Restrictions:
             target_environment
         )
 
+    def _get_boto3_se_client(self, variable_cache: VariableCache=VariableCache(), target_environment: str='default'):
+        boto3_session_base_name = 'AwsBoto3Session:{}:{}'.format(
+            self.spec['awsBoto3Session'],
+            target_environment
+        )
+        if variable_cache.get_value(
+            variable_name='{}:CONNECTED'.format(boto3_session_base_name),
+            value_if_expired=False,
+            default_value_if_not_found=False,
+            raise_exception_on_expired=False,
+            raise_exception_on_not_found=False
+        ) is False:
+            raise Exception('Boto3 Session Does not exist or is not connected yet. Ensure a Boto3 Session is a dependant of this manifest and that the environments match.')
+        
+        boto3_session = variable_cache.get_value(
+            variable_name='{}:SESSION'.format(boto3_session_base_name),
+            value_if_expired=False,
+            default_value_if_not_found=False,
+            raise_exception_on_expired=False,
+            raise_exception_on_not_found=False
+        )
+        if boto3_session:
+            return boto3_session.client('s3')
+        raise Exception('Unable to create S3 client')
+    
+    def _set_variables(self, all_ok: bool=True, checksum_differences_detected: bool=False, variable_cache: VariableCache=VariableCache(), target_environment: str='default'):
+        result_txt = 'ALL_OK'
+        if all_ok is False:
+            result_txt = 'NOT_OK'
+        variable_cache.store_variable(
+            variable=Variable(
+                name='{}:SYNC_RESULT'.format(self._var_name(target_environment=target_environment)),
+                initial_value=result_txt,
+                logger=self.logger
+            ),
+            overwrite_existing=True
+        )
+        variable_cache.store_variable(
+            variable=Variable(
+                name='{}:CHECKSUM_DIFFERENCES_DETECTED'.format(self._var_name(target_environment=target_environment)),
+                initial_value=checksum_differences_detected,
+                logger=self.logger
+            ),
+            overwrite_existing=True
+        )
+
     def implemented_manifest_differ_from_this_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders())->bool:
         if target_environment not in self.metadata['environments']:
             return False
@@ -63,6 +109,12 @@ Restrictions:
             self.log(message='Target environment "{}" not relevant for this manifest'.format(target_environment), level='warning')
             return
         self.log(message='APPLY CALLED', level='info')
+
+        if self.implemented_manifest_differ_from_this_manifest(manifest_lookup_function=manifest_lookup_function, variable_cache=variable_cache, target_environment=target_environment, value_placeholders=value_placeholders) is False:
+            self.log(message='    Bucket "{}" in environment "{}" already appears to be synchronized'.format(self.spec['name'], target_environment), level='info')
+            self._set_variables(oll_ok=True, checksum_differences_detected=False, variable_cache=variable_cache, target_environment=target_environment)
+            return
+
         return 
     
     def delete_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False, target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders()):
