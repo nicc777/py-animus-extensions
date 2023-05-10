d816d229e972adc54256f87016d865a76ee6f7ee
Nico Coetzee <nicc777@gmail.com>
2023-05-10 06:32:06 +0200

    doc: #8 - Started with implementation logic

--- implementations/web-download-file-v1.py
+++ implementations/web-download-file-v1.py
@@ -40,13 +40,34 @@ The destination file with ful path will be stored in the `Variable` named `:FILE
             self.log(message='EXCEPTION: {}'.format(traceback.format_exc()), level='error')
         raise Exception('Failed to get content length of URL "{}"'.format(url))
 
+    def _set_variables(self, all_ok: bool=True, variable_cache: VariableCache=VariableCache(), target_environment: str='default'):
+        result_txt = 'SUCCESS'
+        if all_ok is False:
+            result_txt = 'FAIL'
+        variable_cache.store_variable(
+            variable=Variable(
+                name='{}:STATUS'.format(self._var_name(target_environment=target_environment)),
+                initial_value=result_txt,
+                logger=self.logger
+            ),
+            overwrite_existing=True
+        )
+        variable_cache.store_variable(
+            variable=Variable(
+                name='{}:FILE_PATH'.format(self._var_name(target_environment=target_environment)),
+                initial_value=self.spec['targetOutputFile'],
+                logger=self.logger
+            ),
+            overwrite_existing=True
+        )
+
     def implemented_manifest_differ_from_this_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders())->bool:
         if target_environment not in self.metadata['environments']:
             return False
         
         # Check if the local file exists:
         if os.path.exists(self.spec['targetOutputFile']) is True:
-            if Path.is_file(self.spec['targetOutputFile']) is True:
+            if Path(self.spec['targetOutputFile']).is_file() is True:
                 local_file_size = int(get_file_size(file_path=self.spec['targetOutputFile']))
                 remote_file_size = self._get_url_content_length(url=self.spec['sourceUrl'])
                 self.log(message='local_file_size={}   remote_file_size={}'.format(local_file_size, remote_file_size), level='info')
@@ -64,9 +85,17 @@ The destination file with ful path will be stored in the `Variable` named `:FILE
             self.log(message='Target environment "{}" not relevant for this manifest'.format(target_environment), level='warning')
             return
         self.log(message='APPLY CALLED', level='info')
+
+        url = self.spec['sourceUrl']
+        target_file = self.spec['targetOutputFile']
+
         if self.implemented_manifest_differ_from_this_manifest(manifest_lookup_function=manifest_lookup_function, variable_cache=variable_cache, target_environment=target_environment, value_placeholders=value_placeholders) is False:
-            self.log(message='   URL "{}" appears to be already downloaded to target file "{}"'.format(self.spec['sourceUrl'], self.spec['targetOutputFile']), level='info')
+            self._set_variables(all_ok=True, variable_cache=variable_cache, target_environment=target_environment)
+            self.log(message='   URL "{}" appears to be already downloaded to target file "{}"'.format(url, target_file), level='info')
             return
+        self.log(message='   Downloading URL "{}" to target file "{}"'.format(url, target_file), level='info')
+
+
         return 
     
     def delete_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False, target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders()):
@@ -74,4 +103,5 @@ The destination file with ful path will be stored in the `Variable` named `:FILE
             self.log(message='Target environment "{}" not relevant for this manifest'.format(target_environment), level='warning')
             return
         self.log(message='DELETE CALLED', level='info')
+        
         return 
