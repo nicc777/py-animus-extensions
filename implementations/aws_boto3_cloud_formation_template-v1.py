a9bbc376de6c376335c50c942df6f8830fd0a60b
Nico Coetzee <nicc777@gmail.com>
2023-06-23 05:50:31 +0200

    feat: #4 - Logic implementation - Boto3 CloudFormation Client

--- implementations/aws_boto3_cloud_formation_template-v1.py
+++ implementations/aws_boto3_cloud_formation_template-v1.py
@@ -54,9 +54,37 @@ References:
             target_environment
         )
 
+    def _get_boto3_cloudformation_client(self, variable_cache: VariableCache=VariableCache(), target_environment: str='default'):
+        boto3_session_base_name = 'AwsBoto3Session:{}:{}'.format(
+            self.spec['awsBoto3SessionReference'],
+            target_environment
+        )
+        if variable_cache.get_value(
+            variable_name='{}:CONNECTED'.format(boto3_session_base_name),
+            value_if_expired=False,
+            default_value_if_not_found=False,
+            raise_exception_on_expired=False,
+            raise_exception_on_not_found=False
+        ) is False:
+            raise Exception('Boto3 Session Does not exist or is not connected yet. Ensure a Boto3 Session is a dependant of this manifest and that the environments match.')
+
+        boto3_session = variable_cache.get_value(
+            variable_name='{}:SESSION'.format(boto3_session_base_name),
+            value_if_expired=False,
+            default_value_if_not_found=False,
+            raise_exception_on_expired=False,
+            raise_exception_on_not_found=False
+        )
+        if boto3_session:
+            return boto3_session.client('cloudformation')
+        raise Exception('Unable to create CloudFormation client')
+
     def implemented_manifest_differ_from_this_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders())->bool:
         if target_environment not in self.metadata['environments']:
             return False
+
+        cloudformation_client = self._get_boto3_cloudformation_client(variable_cache=variable_cache, target_environment=target_environment)
+
         return False
 
     def apply_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False, target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders()):
@@ -64,6 +92,9 @@ References:
             self.log(message='Target environment "{}" not relevant for this manifest'.format(target_environment), level='warning')
             return
         self.log(message='APPLY CALLED', level='info')
+
+        cloudformation_client = self._get_boto3_cloudformation_client(variable_cache=variable_cache, target_environment=target_environment)
+
         return
 
     def delete_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False, target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders()):
@@ -71,4 +102,7 @@ References:
             self.log(message='Target environment "{}" not relevant for this manifest'.format(target_environment), level='warning')
             return
         self.log(message='DELETE CALLED', level='info')
+
+        cloudformation_client = self._get_boto3_cloudformation_client(variable_cache=variable_cache, target_environment=target_environment)
+
         return
