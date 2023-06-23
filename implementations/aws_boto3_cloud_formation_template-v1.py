2582f563fe16425f6a81b629824f2ed4617198dd
Nico Coetzee <nicc777@gmail.com>
2023-06-23 06:23:51 +0200

    feat: #4 - Logic implementation - wip

--- implementations/aws_boto3_cloud_formation_template-v1.py
+++ implementations/aws_boto3_cloud_formation_template-v1.py
@@ -1,9 +1,8 @@
 
 from py_animus.manifest_management import *
 from py_animus import get_logger, get_utc_timestamp
-
-
-
+import re
+import boto3
 
 class AwsBoto3CloudFormationTemplate(ManifestBase):
     """Creates a new stack or applies a changeset in AWS CloudFormation.
@@ -79,11 +78,129 @@ References:
             return boto3_session.client('cloudformation')
         raise Exception('Unable to create CloudFormation client')
 
+    def _set_variables(
+            self,
+            variable_cache: VariableCache=VariableCache(),
+            target_environment: str='default',
+            status: str='UNKNOWN',
+            is_status_final: bool=False,
+            local_template_checksum: str=None,
+            remote_template_checksum: str=None,
+            outputs: dict=dict(),
+            resources: dict=dict()
+        ):
+        if is_status_final is True:
+            variable_cache.store_variable(
+                variable=Variable(
+                    name='{}:FINAL_STATUS'.format(self._var_name(target_environment=target_environment)),
+                    initial_value=status,
+                    logger=self.logger,
+                    mask_in_logs=False
+                ),
+                overwrite_existing=True
+            )
+            variable_cache.store_variable(
+                variable=Variable(
+                    name='{}:PROCESSING_STATUS'.format(self._var_name(target_environment=target_environment)),
+                    initial_value='DONE',
+                    logger=self.logger,
+                    mask_in_logs=False
+                ),
+                overwrite_existing=True
+            )
+        else:
+            variable_cache.store_variable(
+                variable=Variable(
+                    name='{}:PROCESSING_STATUS'.format(self._var_name(target_environment=target_environment)),
+                    initial_value=status,
+                    logger=self.logger,
+                    mask_in_logs=False
+                ),
+                overwrite_existing=True
+            )
+        variable_cache.store_variable(
+            variable=Variable(
+                name='{}:LOCAL_TEMPLATE_CHECKSUM'.format(self._var_name(target_environment=target_environment)),
+                initial_value=local_template_checksum,
+                logger=self.logger,
+                mask_in_logs=False
+            ),
+            overwrite_existing=True
+        )
+        variable_cache.store_variable(
+            variable=Variable(
+                name='{}:REMOTE_TEMPLATE_CHECKSUM'.format(self._var_name(target_environment=target_environment)),
+                initial_value=remote_template_checksum,
+                logger=self.logger,
+                mask_in_logs=False
+            ),
+            overwrite_existing=True
+        )
+        variable_cache.store_variable(
+            variable=Variable(
+                name='{}:OUTPUTS'.format(self._var_name(target_environment=target_environment)),
+                initial_value=outputs,
+                logger=self.logger,
+                mask_in_logs=False
+            ),
+            overwrite_existing=True
+        )
+        variable_cache.store_variable(
+            variable=Variable(
+                name='{}:RESOURCES'.format(self._var_name(target_environment=target_environment)),
+                initial_value=resources,
+                logger=self.logger,
+                mask_in_logs=False
+            ),
+            overwrite_existing=True
+        )
+
+    def _calculate_dict_checksum(self, data: dict=dict())->str:
+        pass
+
+    def _stack_exists(self, client, stack_name: str)->bool:
+        return False
+
+    def _delete_stack(self, client, stack_name: str):
+        pass
+
+    def _get_current_remote_stack_status(
+            self,
+            cloudformation_client,
+            VariableCache=VariableCache(),
+            target_environment: str='default'
+        )->dict:
+        pass
+
+    def _format_template_name_as_stack_name(self)->str:
+        tmp_name = re.sub('[^0-9a-zA-Z]+', '_', self.metadata['name'])
+        name = ''
+        for word in tmp_name.split('_'):
+            if len(word) > 0:
+                name = '{}{}'.format(name, word.capitalize())
+        return name
+
     def implemented_manifest_differ_from_this_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders())->bool:
         if target_environment not in self.metadata['environments']:
             return False
 
         cloudformation_client = self._get_boto3_cloudformation_client(variable_cache=variable_cache, target_environment=target_environment)
+        current_processing_status = variable_cache.get_value(
+            variable_name='{}:PROCESSING_STATUS'.format(self._var_name(target_environment=target_environment)),
+            value_if_expired='NOT_STARTED',
+            default_value_if_not_found='NOT_YET_CHECKED',
+            raise_exception_on_expired=False,
+            raise_exception_on_not_found=False
+        )
+
+        action_get_remote_stack_data = False
+        action_calculate_local_template_checksum = False
+
+        if current_processing_status == 'NOT_STARTED':
+            action_get_remote_stack_data = True
+            action_calculate_local_template_checksum = True
+        elif current_processing_status == 'DONE':
+            return False
 
         return False
 
@@ -95,6 +212,14 @@ References:
 
         cloudformation_client = self._get_boto3_cloudformation_client(variable_cache=variable_cache, target_environment=target_environment)
 
+        if self.implemented_manifest_differ_from_this_manifest(
+            manifest_lookup_function=manifest_lookup_function,
+            variable_cache=variable_cache,
+            target_environment=target_environment,
+            value_placeholders=value_placeholders
+        ) is False:
+            return
+
         return
 
     def delete_manifest(self, manifest_lookup_function: object=dummy_manifest_lookup_function, variable_cache: VariableCache=VariableCache(), increment_exec_counter: bool=False, target_environment: str='default', value_placeholders: ValuePlaceHolders=ValuePlaceHolders()):
@@ -104,5 +229,9 @@ References:
         self.log(message='DELETE CALLED', level='info')
 
         cloudformation_client = self._get_boto3_cloudformation_client(variable_cache=variable_cache, target_environment=target_environment)
+        stack_name = self._format_template_name_as_stack_name()
+
+        if self._stack_exists(client=cloudformation_client, stack_name=stack_name) is True:
+            self._delete_stack(client=cloudformation_client, stack_name=stack_name)
 
         return
